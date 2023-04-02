a28c32bcd5a98440056a743a786d25d7cc3ed614
Eduardo Mury Assafin <101987520+Dudzz01@users.noreply.github.com>
2023-04-02 13:47:55 -0300

    update Criacao Animacao Tutorial

---
+++ Assets/Scripts/ScriptsUi/CanvasOne.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class CanvasOne : MonoBehaviour
+{   
+    [Header("Preferences")]
+
+    [SerializeField] GameObject player1,player2,player3;
+    [SerializeField] Image blackCanvas;
+    [SerializeField] Button mouseClick;
+
+    private void Start()
+    {
+        blackCanvas.DOFade(0f,1f).OnComplete(()=>
+        {
+            showPresentation();
+        });
+    }
+
+    void showPresentation()
+    {
+        Sequence intro = DOTween.Sequence();
+        intro.SetEase(Ease.Linear);
+
+        intro.AppendCallback(()=>
+        {
+            player1.GetComponent<Animator>().SetBool("isWalking",true);
+            player2.GetComponent<Animator>().SetBool("isJumping",true);
+            player3.GetComponent<Animator>().SetBool("isSliding",true);
+
+        });
+    }
+
+    void ButtonClick()
+    {
+        //mouseClick.onClick.AddListener();
+    }
+    
+
+    
+}
