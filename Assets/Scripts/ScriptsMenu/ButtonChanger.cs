96763c4078a1d89fcdd972636da4407d9ce70e79
Lucas Lima <lucas_lima_14@outlook.com>
2023-04-03 02:53:12 -0300

    Menu update
    - Added menu.
    - Created credits scene.

---
+++ Assets/Scripts/ScriptsMenu/ButtonChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonChanger : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] Button button;
+
+    [Header("Properties")]
+    [SerializeField] string sceneName;
+
+    void Start()
+    {
+        button.onClick.AddListener(ChangeToScene);    
+    }
+
+    public void ChangeToScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
