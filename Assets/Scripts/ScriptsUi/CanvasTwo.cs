ba5f65a0c56ec23bce2916a67a96dbbe76e71ac4
Lucas Lima <lucas_lima_14@outlook.com>
2023-04-03 00:30:35 -0300

    Update Controls Animations

--- Assets/Scripts/ScriptsUi/CanvasTwo.cs
+++ Assets/Scripts/ScriptsUi/CanvasTwo.cs
@@ -8,19 +8,22 @@ using UnityEngine.SceneManagement;
 
 public class CanvasTwo : MonoBehaviour
 {
-
-    [Header("Preferences")]
-
-    [SerializeField] GameObject player4,player5,player6,enemy,diamond;
+    [Header("References")]
+    [SerializeField] GameObject player4;
+    [SerializeField] GameObject player5;
+    [SerializeField] GameObject player6;
+    [Space(10f)]
+    [SerializeField] GameObject enemy;
+    [SerializeField] GameObject diamond;
     [SerializeField] Image blackCanvas;
     [SerializeField] SpriteRenderer bullet;
     
     private void Start() 
     {
-        closeFirstPanel();
+        CloseFirstPanel();
     }
 
-   public void closeFirstPanel()
+    public void CloseFirstPanel()
     {
         Sequence ndCanvas = DOTween.Sequence();
 
@@ -30,18 +33,22 @@ public class CanvasTwo : MonoBehaviour
 
         ndCanvas.AppendCallback(()=>
         {
-            
-            player4.GetComponent<Animator>().SetBool("isWalking",false);
-            player6.transform.DOMoveX(player6.transform.position.x+10,1f).OnComplete(()=>diamond.GetComponent<Image>().DOFade(0f,1f));
-            bullet.transform.DOMoveX(-9.76f,1f).OnComplete(()=> {
+            player4.GetComponent<Animator>().SetBool("isWalking", false);
+            player6.GetComponent<Animator>().SetBool("isWalking", true);
+            player6.transform.DOMoveX(0f, 1f).OnComplete(()=> 
+            {
+                player6.GetComponent<Animator>().SetBool("isWalking", false);
+                diamond.GetComponent<Image>().DOFade(0f, 1f);
+            });
+
+            bullet.transform.DOMoveX(-9.76f, 1f).OnComplete(()=> 
+            {
                 player5.GetComponent<Animator>().SetTrigger("dead");
                 bullet.DOFade(0f,0.5f);
-                                                                });
-
+            });
         });
 
         ndCanvas.OnComplete(()=> StartCoroutine(passProxScene()));
-
     }
 
     IEnumerator passProxScene()
@@ -50,5 +57,4 @@ public class CanvasTwo : MonoBehaviour
         SceneManager.LoadScene("Introduction");
         yield return null;
     }
-
 }
