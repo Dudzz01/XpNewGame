5031957fd1f2644f47a2b2389a258618e74047aa
Lucas Lima <lucas_lima_14@outlook.com>
2023-04-12 18:11:27 -0300

    Level 3 - Update
    - Hierarchy cleaning.
    - Added "DiamondCounter" prefab.
    - Added game icon.
    - Main menu texture button update.
    - Selected button small animation.

--- Assets/Scripts/Menu/ChooseOptionsMenu.cs
+++ Assets/Scripts/Menu/ChooseOptionsMenu.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
+using TMPro;
 
-public class ChooseOptionsMenu : MonoBehaviour
+public class ChooseOptionsMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [Header("References")]
     [SerializeField] Image gameName;
@@ -23,10 +25,12 @@ public class ChooseOptionsMenu : MonoBehaviour
         seq.SetLoops(-1, LoopType.Yoyo);
 
         Vector3 myRot = new Vector3(0f, 0f, 5.125f);
+        Vector3 negRot = new Vector3(0f, 0f, -5.125f);
 
         seq.Append(lightFocus.DOFade(.5f, 1f).SetEase(Ease.InOutBounce));
-        seq.Join(gameName.rectTransform.DOLocalRotate(myRot, 1f).SetEase(Ease.InOutSine).OnComplete(()=> gameName.rectTransform.DOLocalRotate(-myRot, 1f).SetEase(Ease.InOutSine)));
-        seq.Join(gameName.rectTransform.DOLocalMoveY(18f, 1f).SetEase(Ease.Linear));
+
+        seq.Join(gameName.rectTransform.DOLocalRotate(myRot, 1f).SetEase(Ease.InOutSine));
+        seq.Append(gameName.rectTransform.DOLocalRotate(negRot, 1f).SetEase(Ease.InOutSine));
     }
 
     public void SelectPlay()
@@ -34,12 +38,12 @@ public class ChooseOptionsMenu : MonoBehaviour
         SceneManager.LoadScene("Level1"); // Dudu: Setar os nomes das cenas quando forem criadas
     }
 
-     public void TutorialComandos()
-     {
+    public void Tutorial()
+    {
         SceneManager.LoadScene("TutorialOne");  // Dudu: Setar os nomes das cenas quando forem criadas
-     }
+    }
 
-    public void Creditos()
+    public void Credits()
     {
         SceneManager.LoadScene("Credits"); // Dudu: Setar os nomes das cenas quando forem criadas
     }
@@ -47,5 +51,30 @@ public class ChooseOptionsMenu : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
+    }
+
+    void ChangeButtonTextColor()
+    {
+
+    }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+        if(data != null)
+        {
+            var myText = data.pointerEnter.GetComponent<TextMeshProUGUI>();
+
+            myText.transform.parent.DOLocalMoveX(-100f, .5f).SetEase(Ease.Linear);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData data)
+    {
+        if(data != null)
+        {
+            var myText = data.pointerEnter.GetComponent<TextMeshProUGUI>();
+
+            myText.transform.parent.DOLocalMoveX(-128f, .5f).SetEase(Ease.Linear);
+        }
     }
 }
