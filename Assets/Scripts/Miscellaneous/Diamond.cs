948e25a2885e28370b8410b65cbc69300edd86c3
Lucas Lima <lucas_lima_14@outlook.com>
2023-04-23 12:47:08 -0300

    Diamond Polish

---
+++ Assets/Scripts/Miscellaneous/Diamond.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Diamond : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Sequence floatingSeq = DOTween.Sequence();
+
+        floatingSeq.SetLoops(-1, LoopType.Yoyo);
+        floatingSeq.SetEase(Ease.Linear);
+
+        float dur = 0.25f;
+
+        floatingSeq.Append(transform.DOLocalMoveY(transform.position.y - 0.1f, dur));
+    }
+}
