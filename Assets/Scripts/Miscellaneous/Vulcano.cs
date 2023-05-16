fabb97932fbcdc8b78d5ade61e4288f1836d23b1
Lucas Lima <lucas_lima_14@outlook.com>
2023-05-16 16:35:00 -0300

    Update - [Tilemap & Intro]
    - Re-organized tilemaps.
    - Added "Vulcano Games Studio" video intro.
    - Root folder cleaning.

---
+++ Assets/Scripts/Miscellaneous/Vulcano.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class Vulcano : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += OnEndReached;
+    }
+
+    void OnEndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
